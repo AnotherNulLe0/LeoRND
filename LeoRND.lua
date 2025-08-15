b2dee5932db5497ce5f6425c7843700fcee21d0c
AnotherNulLe0 <sovm.pokupki@gmail.com>
2025-08-15 12:00:25 +0700

    Added debugplus support

--- LeoRND.lua
+++ LeoRND.lua
@@ -5,6 +5,80 @@ LeoRND.utils = SMODS.load_file("utils.lua")()
 LeoRND.config = SMODS.load_file("config.lua")()
 SMODS.load_file("lib/hooks.lua")()
 SMODS.load_file("lib/ownerships.lua")()
+-- Additions to blinds
+SMODS.load_file("lib/blinds.lua")()
+
+-- Debug+ api
+local success, dpAPI = pcall(require, "debugplus-api")
+
+if success and dpAPI.isVersionCompatible(1) then
+    local debugplus = dpAPI.registerID("MyMod")
+	debugplus.addCommand({
+	    name = "curse",
+	    shortDesc = "Set or add to your curse",
+	    desc = "(Must have a curse counter present) Set or add to your curse. Usage:\ncurse set [amount] - Set the current curse to the given amount\ncurse add [amount] - Adds the given number of curse.",
+	    exec = function (args, rawArgs, dp)
+	        if G.STAGE ~= G.STAGES.RUN then
+        	    return "This command must be run during a run.", "ERROR"
+        	end
+			if not G.GAME.modifiers.enable_cursed then
+				return "This command must be run during a run with curses enabled", "ERROR"
+			end
+        	local subCmd = args[1]
+        	local amount = tonumber(args[2])
+        	if subCmd == "set" then
+        	    if not amount then
+        	        return "Please provide a valid number to set/add.", "ERROR"
+        	    end
+        	    ease_curse(amount - G.GAME.curse)
+				return "Curse is now " .. amount
+        	elseif subCmd == "add" then
+        	    if not amount then
+        	        return "Please provide a valid number to set/add.", "ERROR"
+        	    end
+        	    ease_curse(amount)
+				return "Curse is now " .. G.GAME.curse + amount
+        	else
+        	    return "Please choose whether you want to add or set. For more info, run 'help curse'"
+        	end
+	    end
+	})
+	debugplus.addCommand({
+	    name = "blind",
+	    shortDesc = "Reroll upcoming blinds",
+	    desc = "Reroll upcoming blinds. Usage:\n\
+				blind [kind] reroll - rerolls selected blind kind.\n\
+				blind [kind] set [blind] - replaces [kind]blind with a specified blind.\n\
+				Possible blind kinds: 'small', 'big' and 'boss'",
+	    exec = function (args, rawArgs, dp)
+			if G.STAGE ~= G.STAGES.RUN then
+        	    return "This command must be run during a run.", "ERROR"
+        	end
+        	local subCmd = args[2]
+        	local kind = args[1]
+        	local replaceWith = args[3]
+			if subCmd == "reroll" then
+        		if kind == "small" then
+        		    G.FUNCS.reroll_small_blind()
+        		elseif kind == "big" then
+        		    G.FUNCS.reroll_big_blind()
+        		elseif kind == "boss" then
+					G.from_boss_tag = true
+        		    G.FUNCS.reroll_boss()
+        		else
+        		    return "Uknown kind of blind: '"..kind.."'.Possible blind kinds: 'small', 'big' and 'boss'", "ERROR"
+        		end
+				return "Rerolling "..kind.." blind."
+			elseif subCmd == "set" then
+				G.FUNCS.set_blind(kind, replaceWith)
+				return "Replacing "..kind.." with "..replaceWith.."."
+			else
+				return "Please choose whether you want to reroll or set. For more info, run 'help blind'"
+			end
+
+	    end
+	})
+end
 
 SMODS.Atlas {
 	key = "jokers",
