898db3ac2f6077174e22c5ac163221667cbada0c
AnotherNulLe0 <sovm.pokupki@gmail.com>
2025-08-15 13:48:32 +0700

    Added first curse effect: bigger regular blinds

--- items/blinds/non_final.lua
+++ items/blinds/non_final.lua
@@ -17,14 +17,22 @@ local cursed_small = {
     key = "cursed_small",
     small = true,
     pos = { x = 0, y = 0 },
-    mult = 2
+    mult = 1.5,
+    boss_colour = mix_colours(mix_colours(G.C.BLUE, G.C.BLACK, 0.6), G.C.PURPLE, 0.4),
+    in_pool = function (self)
+        return LeoRND.utils.poll_curse_effect("smallblind", 5, G.GAME.curse + 2, 1.2)
+    end
 }
 
 local cursed_big = {
     key = "cursed_big",
     big = true,
     pos = { x = 0, y = 1 },
-    mult = 3
+    mult = 2,
+    boss_colour = mix_colours(mix_colours(G.C.ORANGE, G.C.BLACK, 0.6), G.C.PURPLE, 0.4),
+    in_pool = function (self)
+        return LeoRND.utils.poll_curse_effect("bigblind", 5, G.GAME.curse + 1, 1.2)
+    end
 }
 
 return {
